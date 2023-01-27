f19b7b7b5e10cacda61b00468dd7ddf5a40ef6ac
MY <MY@DESKTOP-J4GA5NP.comnect.local>
2023-01-27 12:28:06 +0530

    done1

--- src/Chinya/sagar.java
+++ src/Chinya/sagar.java
@@ -1,7 +1,9 @@
 package Chinya;
 
 public class sagar {
-public static void main(String[] args) {
-	System.out.println("done");
-}
+	public static void main(String[] args) {
+		System.out.println("done");
+		System.out.println("done1");
+	}
+
 }
